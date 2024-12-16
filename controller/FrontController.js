ef75d33c11f8c0ead63a29b04f6a867c2dda9995
nirpal rawat <nirpalrawat@gmail.com>
2024-12-17 00:24:54 +0530

    jhh

--- controller/FrontController.js
+++ controller/FrontController.js
@@ -120,7 +120,7 @@ class FrontController {
         html:
           "<p>Hii " +
           name +
-          ',Please click here to <a href="http://localhost:3000/reset-password?token=' +
+          ',Please click here to <a href="https://blogproject-mcsw.onrender.com/reset-password?token=' +
           token +
           '">Reset</a>Your Password.',
       });
